5afc4cb50b42d98e745ecc1ae5ff9f899d68e89a
mysbi <mysbi@LAPTOP-T4TNM0R0.wusm-wifi.wucon.wustl.edu>
2024-09-04 14:20:42 -0500

    Finished 3/5

--- src/studio1/HiOne.java
+++ src/studio1/HiOne.java
@@ -11,7 +11,8 @@ public class HiOne {
 
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter value for arg 0: ");
-		String name = in.nextLine();
+		String name = in.nextLine("John");
+
 		//
 		// Below this line, enter code so that this program's output says
 		//      Hi, Pat.  How are you?
--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,8 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		boolean isOrdered = (x < y && x < z && y < z || x > y && y > z && x > z);
+		System.out.println(isOrdered);
 	}
 
 }
